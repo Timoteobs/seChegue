cf7dc232384244cd1afbad04084cb364f4157bdb
Emerson Souza <40323952+EmesonSSouza@users.noreply.github.com>
2020-10-10 19:46:03 -0300

    criação da classe lifeProject e ajustes na classe user

--- backend/data/model/user.model.ts
+++ backend/data/model/user.model.ts
@@ -4,20 +4,10 @@ export default class User extends Model {
     id!: number;
     name: string;
     userName: string;
-    wishes: string[];
-    personalTraits: string[]; //qualidades,defeitos,qualidade que gostaria de ter
-    checklistDay: string;
-    bio: string;
-    img: any[];
 
     constructor() {
         super();
     }
-
-    ToModify() {
-        return this.toJSON();
-    }
-
 
     setName(name) {
         this.name = name;
@@ -27,23 +17,7 @@ export default class User extends Model {
         this.userName = userName;
     }
 
-    setWishes(wishes) {
-        this.wishes = wishes;
-    }
-
-    setPersonalTraits(personalTraits) {
-        this.personalTraits = personalTraits;
-    }
-
-    setChecklistDay(checklistDay) {
-        this.checklistDay = checklistDay;
-    }
-
-    setBio(bio) {
-        this.bio = bio;
-    }
-
-    setImg(img) {
-        this.img = img;
+    ToModify() {
+        return this.toJSON();
     }
 }
