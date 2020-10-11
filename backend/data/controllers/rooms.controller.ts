03aa29baa3f26be9fb7543de04d97e8a9c678386
Micaele <micaelemariliaalves2@hotmail.com>
2020-10-11 12:33:53 -0300

    Crianção dos controllers

--- backend/data/controllers/rooms.controller.ts
+++ backend/data/controllers/rooms.controller.ts
@@ -3,14 +3,15 @@ import { controller, httpGet, httpPost, httpDelete, request, response, httpPut }
 import { IRoomsService } from "../interfaces/IServices/roomsServices.interface";
 import Rooms from "../model/rooms.model";
 import TYPES from "../types";
+import * as express from 'express';
 
 @controller('')
 class RoomsController {
 
-  constructor(@inject(TYPES.IroomsService) private service: IRoomsService) { }
+  constructor(@inject(TYPES.IRoomsService) private service: IRoomsService) { }
 
   @httpPost('/rooms')
-  post(@request() req: Request<any>, @response() res: Response<any>): Promise<any> {
+  post(@request() req: express.Request<any>, @response() res: express.Response<any>): Promise<any> {
     return new Promise((resolve) => {
       this.service.save(new Rooms(req.body))
         .then((result: any) => resolve(result))
